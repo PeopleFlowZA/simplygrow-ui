da595e7fba0b91fb8df445c382d308f92ceb215e
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-07-30 11:29:53 +0200

    Add GridContainer component

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -1,3 +1,4 @@
 export { default as FlexContainer } from './flex-container';
+export { default as GridContainer } from './grid-container';
 export { default as Heading } from './heading';
 export { default as Paragraph } from './paragraph';
